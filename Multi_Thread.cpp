7b05a7de805845ede089b33529e8fd212675dfb0
生我材 天 <1186518346@qq.com>
2020-05-10 16:41:10 +0800

    多线程的实现

--- Multi_Thread.cpp
+++ Multi_Thread.cpp
@@ -1,9 +1,19 @@
 ﻿#include "Multi_Thread.h"
 using namespace std;
+//#include <Windows.h>
+//#include<sysinfoapi.h>
+//#pragma(lib,"Kernel32.lib")
 
 int main()
 {
+	
+	/*SYSTEM_INFO sysInfo;
+	GetSystemInfo(&sysInfo);
+	ULONG  numOfThread = sysInfo.dwNumberOfProcessors;
+	cout << "最佳的线程数目是：" << numOfThread << endl;*/
+
 	threadPool();
+
 
 	return 0;
 }
